67ce4ccc065e52054b6a42dde5d41e26713238f8
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-03-26 21:46:58 +0000

    .

--- react-examples/src/App.js
+++ react-examples/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
-import logo from './logo.svg';
+import Timer from './hooks/useEffectEx.js'
+
 import './App.css';
 
 // import FavoriteColor from './hooks/useStateEx';
@@ -24,18 +25,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>test</h1>
+
+        <Timer></Timer>
+
+
       </header>
     </div>
   );
---
+++ react-examples/src/hooks/useEffectEx.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+function Timer() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+  });
+
+  return <h1>I've rendered {count} times!</h1>;
+}
+
+
+export default Timer();
+
