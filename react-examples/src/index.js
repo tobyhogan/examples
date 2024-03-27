19d1ec3dbe8bde11a3e0f3ff462bf9041515976d
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-03-27 19:35:01 +0000

    .

---
+++ react-examples/src/index.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+
+function FavoriteColor() {
+  const [color, setColor] = useState("red");
+
+  return <h1>My favorite color is {color}!</h1>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<FavoriteColor />);
