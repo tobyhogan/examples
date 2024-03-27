2e8089f67e39b8b47edb99f62221c1e0821f31b2
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-03-27 19:43:22 +0000

    .

--- react-examples/src/hooks/useEffectEx/index.js
+++ react-examples/src/hooks/useEffectEx/index.js
@@ -1,3 +1,14 @@
+//USE EFFECT HOOKS
+
+/* 
+- useEffect accepts two arguments(function and dependency),
+    the second is optional, though you should usually pass it
+    the second parameter is an array which an have prop or state values
+- useEffect runs on every render, to get it to not do this and only run on
+    the initial render you can pass in anything(e.g. empty brackets, implying
+      no dependencies) to the depency argument
+*/
+
 import { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 
